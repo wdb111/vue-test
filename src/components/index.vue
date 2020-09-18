772a673121917097614166ee13ad13fcb04c40e3
wdb111 <1303329454@qq.com>
2020-09-18 16:39:54 +0800

    9.18

--- src/components/index.vue
+++ src/components/index.vue
@@ -3,90 +3,92 @@
     <header>
       <h1>主页</h1>
     </header>
-    <el-button v-for="(item,index) in dataList" :key="index" class="button" type="primary" @click="to_page(item.url)">{{item.name}}</el-button>
+    <el-button v-for="(item,index) in dataList" :key="index" class="button" type="primary" @click="to_page(item.url)">
+      {{ item.name }}
+    </el-button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: "index",
-  data() {
+  name: 'index',
+  data () {
     return {
-      dataList:[
+      dataList: [
         {
-          name:"操作数据库",
-          url:"/to_database"
+          name: '操作数据库',
+          url: '/to_database'
         },
         {
-          name:"生成二维码",
-          url:"/to_Erweima"
+          name: '生成二维码',
+          url: '/to_Erweima'
         },
         {
-          name:"elementUI例子",
-          url:"/to_element/el-table"
-        },{
-          name:"iView例子",
-          url:"/to_iview/nav"
+          name: 'elementUI例子',
+          url: '/to_element/el-table'
+        }, {
+          name: 'iView例子',
+          url: '/to_iview/nav'
         },
         {
-          name:"代码高亮编辑器",
-          url:"/to_codemirror"
+          name: '代码高亮编辑器',
+          url: '/to_codemirror'
         },
         {
-          name:"backcTop回到顶部",
-          url:"/to_backTop"
+          name: 'backcTop回到顶部',
+          url: '/to_backTop'
         },
         {
-          name:"显示当前时间",
-          url:"/to_date"
+          name: '显示当前时间',
+          url: '/to_date'
         },
         {
-          name:"less的使用",
-          url:"/to_less"
+          name: 'less的使用',
+          url: '/to_less'
         },
         {
-          name:"lodash的使用",
-          url:"/to_lodash"
+          name: 'lodash的使用',
+          url: '/to_lodash'
         },
         {
-          name:"tinymce富文本编辑器",
-          url:"/to_tinymce"
+          name: 'tinymce富文本编辑器',
+          url: '/to_tinymce'
         },
         {
-          name:"wangeditor富文本编辑器",
-          url:"/to_wangeditor"
+          name: 'wangeditor富文本编辑器',
+          url: '/to_wangeditor'
         },
         {
-          name:"验证码",
-          url:"/identify"
+          name: '验证码',
+          url: '/identify'
         },
         {
-          name:"地图",
-          url:"/to_map"
+          name: '地图',
+          url: '/to_map'
         },
         {
-          name:"制作渐变尖角",
-          url:"/to_jiao"
+          name: '制作渐变尖角',
+          url: '/to_jiao'
         },
         {
-          name:"拖拽",
-          url:"/to_draggable"
+          name: '拖拽',
+          url: '/to_draggable'
         },
         {
-          name:"PDF.js",
-          url:"/to_pdf"
+          name: 'PDF.js',
+          url: '/to_pdf'
         }
       ]
-    };
+    }
   },
   methods: {
-    to_page(url){
-      this.$router.push(url);
+    to_page (url) {
+      this.$router.push(url)
     }
-    
+
   }
-};
+}
 </script>
 
 <style scoped>
@@ -99,6 +101,7 @@ header {
   width: 100%;
   height: 100%;
 }
+
 .button {
   margin-top: 20px;
 }
--- src/components/lodash/index.vue
+++ src/components/lodash/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>lodash</h1>
+      <a href="https://www.lodashjs.com/" target="_blank">Lodash中文文档</a>
     </div>
 </template>
 <script>
@@ -8,7 +9,7 @@ import _ from "lodash";
 export default {
     data () {
         return {
-            
+
         }
     },
     created () {
