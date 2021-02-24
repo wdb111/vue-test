ded79154411ed011f52e0e7a25084d9f736fd32f
wdb111 <1303329454@qq.com>
2021-02-24 18:26:45 +0800

    2.24

--- src/components/BatchAdd/index.vue
+++ src/components/BatchAdd/index.vue
@@ -5,28 +5,176 @@
   版本：v1.0
 -->
 <template>
-    <div>批量添加</div>
+    <div>
+        <Button class="btn" @click="batchAdd">添加</Button>
+        <div class="table-box">
+            <div class="title">
+                <div>
+                    <Checkbox
+                        :indeterminate="indeterminate"
+                        :value="checkAll"
+                        @click.prevent.native="handleCheckAll"
+                    ></Checkbox>
+                </div>
+                <div>ID</div>
+                <div>标题1</div>
+                <div>标题2</div>
+                <div>标题3</div>
+                <div>标题4</div>
+                <div>标题5</div>
+                <div>操作</div>
+            </div>
+            <div class="content">
+                <div class="item" v-for="(row, index) in dataList">
+                    <div>
+                        <Checkbox
+                            @on-change="(val) => changeSelect(val, row)"
+                            v-model="row.selected"
+                        ></Checkbox>
+                    </div>
+                    <div>{{ index + 1 }}</div>
+                    <div>
+                        <Input
+                            v-model="row.value1"
+                            placeholder="Enter something..."
+                        />
+                    </div>
+                    <div>
+                        <i-switch size="large" v-model="row.value2">
+                            <span slot="open">开启</span>
+                            <span slot="close">关闭</span>
+                        </i-switch>
+                    </div>
+                    <div>
+                        <Input
+                            v-model="row.value3"
+                            placeholder="Enter something..."
+                        />
+                    </div>
+                    <div>
+                        <Input
+                            v-model="row.value4"
+                            placeholder="Enter something..."
+                        />
+                    </div>
+                    <div>
+                        <Input
+                            v-model="row.value5"
+                            placeholder="Enter something..."
+                        />
+                    </div>
+                    <div>
+                        <Icon type="md-create" @click="updateRow(row)" />
+                        <Icon type="md-trash" @click="deleteRow(index)" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'demo',
-  props: {},
-  components: {},
-  data () {
-   return {}
-  },
-  filters: {},
-  computed: {},
-  watch: {},
-  methods: {},
-  created () {
-  },
-  mounted () {
-  },
-}
+    name: "demo",
+    props: {},
+    components: {},
+    data() {
+        return {
+            indeterminate: true,
+            checkAll: false,
+            dataList: [],
+        };
+    },
+    filters: {},
+    computed: {},
+    watch: {},
+    methods: {
+        handleCheckAll() {
+            if (this.indeterminate) {
+                this.checkAll = false;
+            } else {
+                this.checkAll = !this.checkAll;
+            }
+            this.indeterminate = false;
+            this.dataList.forEach((item) => {
+                item.selected = this.checkAll;
+            });
+        },
+        changeSelect(val, row) {
+            console.log(val, row);
+        },
+        batchAdd() {
+            let obj = {
+                selected: false,
+                value1: "",
+                value2: false,
+                value3: "",
+                value4: "",
+                value5: "",
+            };
+            this.dataList.push(obj);
+        },
+        updateRow(row) {
+            console.log(row);
+        },
+        deleteRow(index) {
+            this.dataList.splice(index, 1);
+        },
+    },
+    created() {},
+    mounted() {},
+};
 </script> 
 
 <style lang='less' scoped>
+.btn {
+    margin: auto;
+    margin-top: 20px;
+}
+.table-box {
+    margin: 20px auto;
+    width: 60%;
+    height: 600px;
+    background-color: #eee;
+    .title,
+    .item {
+        display: flex;
+        justify-content: space-around;
+        div {
+            width: 170px;
+            height: 40px;
+            text-align: center;
+            line-height: 40px;
+            font-size: 14px;
+            color: #2c344d;
+            font-weight: 600;
+        }
+        & > div:first-child,
+        & > div:nth-child(2) {
+            width: 60px;
+        }
+        & > div:last-child {
+            width: 100px;
+        }
+    }
 
+    .content {
+        height: 560px;
+        overflow-y: auto;
+        .item {
+            margin: 10px 0;
+            div {
+                font-size: 14px;
+                font-weight: 100;
+            }
+            & > div:last-child {
+                font-size: 24px;
+                cursor: pointer;
+                i:hover {
+                    color: rgb(48, 112, 231);
+                }
+            }
+        }
+    }
+}
 </style>
